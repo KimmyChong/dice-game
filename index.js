44f0947d61539f7ad4be7abe5b0aed13f7841999
KimmyChong <165919389+KimmyChong@users.noreply.github.com>
2024-04-05 13:10:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var randomNumber1 = Math.floor(Math.random() * 6 + 1);
 var randomNumber2 = Math.floor(Math.random() * 6 + 1);
 
-var randomDice1 = "dice" + randomNumber1 + ".png";
-var randomDice2 = "dice" + randomNumber2 + ".png";
+var randomDice1 = "/dice" + randomNumber1 + ".png";
+var randomDice2 = "/dice" + randomNumber2 + ".png";
 
 document.querySelector(".img1").setAttribute("src",randomDice1);
 document.querySelector(".img2").setAttribute("src",randomDice2);
